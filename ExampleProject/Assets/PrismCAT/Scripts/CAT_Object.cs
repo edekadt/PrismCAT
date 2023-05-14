5d14c4a16bd5b474b92e8e4e32b7d5e0d8d2a1c0
Emile de Kadt <62763004+edekadt@users.noreply.github.com>
2023-05-14 18:49:47 +0200

    Added transparency to CAT_Object

--- ExampleProject/Assets/PrismCAT/Scripts/CAT_Object.cs
+++ ExampleProject/Assets/PrismCAT/Scripts/CAT_Object.cs
@@ -6,6 +6,10 @@ namespace PrismCAT
 {
     public class CAT_Object : CAT_ColourComponent
     {
+        [Tooltip("Transparency at which the indexed colour is applied over the base image. \n" +
+            "At 0, colour is applied in full opacity. At 1, overlayed colour is fully transparent.")]
+        [SerializeField, Range(0f, 1f)] float colourTransparency;
+
         private Renderer render;
 
         // Initial colour of the object
@@ -36,7 +40,7 @@ namespace PrismCAT
         public override void UpdateColour()
         {
             if (colourManager != null)
-                render.material.color = colourManager.GetColour((int)colour);
+                render.material.color = Color.Lerp(colourManager.GetColour(colour), baseColour, colourTransparency);
         }
     }
 
